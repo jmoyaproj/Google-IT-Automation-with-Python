a56628c1b4d92700c598f3630ef08136c4375efc
jmoyaproj <jmoyaproj@gmail.com>
2023-01-24 12:03:28 -0600

    Added HelloWorld - Practice Quiz.

---
+++ Crash-Course-on-Python/Week-1/Hello-World/PracticeQuiz.py
@@ -0,0 +1,8 @@
+# Question 4
+# Output a message that says "Programming in Python is fun!" to the screen.
+print("Programming in Python is fun!")
+
+# Question 5
+# Calculate the Golden Ratio.
+ratio = (1 + 5 ** (1 / 2)) / 2
+print(ratio)
