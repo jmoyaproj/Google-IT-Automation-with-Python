b00ea4e69e7c4801898d09c61362ee5dd2fdff4a
jmoyaproj <jmoyaproj@gmail.com>
2023-01-26 22:18:17 -0600

    Completed Conditionals Module.

---
+++ Crash-Course-on-Python/Week-2/Conditionals/BranchingWithIfStatements.py
@@ -0,0 +1,6 @@
+# Question
+# The is_positive function should return True if the number received is positive, otherwise
+# it returns None. Can you fill in the gaps to make that happen?
+def is_positive(number):
+  if number > 0:
+    return True
---
+++ Crash-Course-on-Python/Week-2/Conditionals/ElseStatements.py
@@ -0,0 +1,8 @@
+# Question
+# The is_positive function should return True if the number received is positive and False if it isn't.
+# Can you fill in the gaps to make that happen?
+def is_positive(number):
+  if number > 0:
+    return True
+  else:
+    return False
