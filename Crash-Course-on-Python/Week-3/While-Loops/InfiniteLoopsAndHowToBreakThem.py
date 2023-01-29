fc57bf0176df8728f1c565f16621ca1306482189
jmoyaproj <jmoyaproj@gmail.com>
2023-01-28 18:14:29 -0600

    Completed Week - While Loops.

---
+++ Crash-Course-on-Python/Week-3/While-Loops/InfiniteLoopsAndHowToBreakThem.py
@@ -0,0 +1,10 @@
+# Question
+# The following code causes an infinite loop. Can you figure out what’s missing and how to fix it?
+def print_range(start, end):
+	# Loop through the numbers from start to end
+	n = start
+	while n <= end:
+		print(n)
+		n += 1
+
+print_range(1, 5)  # Should print 1 2 3 4 5 (each number on its own line) 
